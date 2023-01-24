1298dee98d077d402763a5bad9f53c0b06d8af5b
Sandy Rachman <sandy.rachman@gmail.com>
2023-01-24 14:20:38 +0700

    feat: improved error handling for missing exchange rates

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,25 +10,38 @@ class Portfolio {
   }
 
   evaluate(currency) {
+    let failures = [];
     let total = this.moneys.reduce((sum, money) => {
-      return sum + this.convert(money, currency);
+      let convertedAmount = this.convert(money, currency);
+      if (convertedAmount === undefined) {
+        failures.push(money.currency + "->" + currency);
+        return sum;
+      }
+      return sum + convertedAmount;
     }, 0);
 
-    return new Money(total, currency);
+    if (!failures.length) {
+      return new Money(total, currency);
+    }
+    throw new Error("Missing exchange rate(s): [" + failures.join() + "]");
   }
 
   convert(money, currency) {
-    // let eurToUsd = 1.2;
     let exchangeRates = new Map();
     exchangeRates.set("EUR->USD", 1.2);
     exchangeRates.set("USD->KRW", 1100);
-    let key = money.currency + "->" + currency;
 
     if (money.currency === currency) {
       return money.amount;
     }
-    // return money.amount * eurToUsd;
-    return money.amount * exchangeRates.get(key);
+
+    let key = money.currency + "->" + currency;
+    let rate = exchangeRates.get(key);
+
+    if (rate === undefined) {
+      return undefined;
+    }
+    return money.amount * rate;
   }
 }
 
--- py/portfolio.py
+++ py/portfolio.py
@@ -11,10 +11,20 @@ class Portfolio:
         self.moneys.extend(moneys)
 
     def evaluate(self, currency):
-        total = functools.reduce(
-            operator.add, map(lambda m: self.__convert(m, currency), self.moneys), 0)
-        return Money(total, currency)
-    
+        total = 0.0
+        failures = []
+        for m in self.moneys:
+            try:
+                total += self.__convert(m, currency)
+            except KeyError as ke:
+                failures.append(ke)
+
+        if len(failures) == 0:
+            return Money(total, currency)
+        
+        failureMessage = ",".join(f.args[0] for f in failures)
+        raise Exception("Missing exchange rate(s):[" + failureMessage + "]")
+
     def __convert(self, aMoney, aCurrency):
         exchangeRates = {
             'EUR->USD': 1.2,
@@ -24,6 +34,5 @@ class Portfolio:
         if aMoney.currency == aCurrency:
             return aMoney.amount
         else:
-            # return aMoney.amount * self._eur_to_usd
             key = aMoney.currency + '->' + aCurrency
             return aMoney.amount * exchangeRates[key]
