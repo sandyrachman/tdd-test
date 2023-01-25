90be0c82754a462a5aa9f8b2ccac4336d2c43dd9
Sandy Rachman <sandy.rachman@gmail.com>
2023-01-25 09:19:07 +0700

    test: verified behavior when modifying an existing exchange rates

--- js/test_money.js
+++ js/test_money.js
@@ -4,7 +4,7 @@ const Portfolio = require("./portfolio");
 const Bank = require("./bank");
 
 class MoneyTest {
-  constructor() {
+  setUp() {
     this.bank = new Bank();
     this.bank.addExchangeRate("EUR", "USD", 1.2);
     this.bank.addExchangeRate("USD", "KRW", 1100);
@@ -73,11 +73,27 @@ class MoneyTest {
     }, expectedError);
   }
 
-  testConversion() {
-    let bank = new Bank();
-    bank.addExchangeRate("EUR", "USD", 1.2);
+  testConversionWithDifferentRatesBetweenTwoCurrencies() {
+    this.bank.addExchangeRate("EUR", "USD", 1.2);
     let tenEuros = new Money(10, "EUR");
-    assert.deepStrictEqual(bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+    assert.deepStrictEqual(
+      this.bank.convert(tenEuros, "USD"),
+      new Money(12, "USD")
+    );
+
+    this.bank.addExchangeRate("EUR", "USD", 1.3);
+    assert.deepStrictEqual(
+      this.bank.convert(tenEuros, "USD"),
+      new Money(13, "USD")
+    );
+  }
+
+  testWhatIsTheConversionRateFromEURToUSD() {
+    let tenEuros = new Money(10, "EUR");
+    assert.deepStrictEqual(
+      this.bank.convert(tenEuros, "USD"),
+      new Money(12, "USD")
+    );
   }
 
   testConversionWithMissingExchangeRate() {
@@ -105,6 +121,7 @@ class MoneyTest {
       console.log("Running: %s()", m);
       let method = Reflect.get(this, m);
       try {
+        this.setUp();
         Reflect.apply(method, this, []);
       } catch (e) {
         if (e instanceof assert.AssertionError) {
